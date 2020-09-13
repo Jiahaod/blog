d9b9e39a6e628d27b20359de55c980dba1a1d68e
Jmzer <ge1145370834@qq.com>
2020-09-13 22:45:40 +0800

    Initial commit

---
+++ post/mycontextprocessor.py
@@ -0,0 +1,19 @@
+#coding=utf-8
+from django.db.models import Count
+
+from post.models import Post
+
+
+def getRightInfo(request):
+
+    #1.获取分类信息
+    r_catepost = Post.objects.values('category__cname','category').annotate(c=Count('*')).order_by('-c')
+    print(r_catepost.count())
+    #2.近期文章
+    r_recpost = Post.objects.all().order_by('-created')[:3]
+    #归档信息
+    from django.db import connection
+    cursor = connection.cursor()
+    cursor.execute("select created,count('*') c from t_post GROUP BY DATE_FORMAT(created,'%Y-%m') ORDER BY c desc,created desc")
+    r_filepost = cursor.fetchall()
+    return {'r_catepost':r_catepost,'r_recpost':r_recpost, 'r_filepost':r_filepost}
---
+++ post/templatetags/myfilter.py
@@ -0,0 +1,9 @@
+from django.template import  Library
+
+register = Library()
+
+@register.filter
+
+def md(value):
+    import markdown
+    return markdown.markdown(value)
